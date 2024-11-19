748e74ab7d6535f01f7b1c971c87d9d6b7c1e40d
euger <azkriven16@gmail.com>
2024-11-19 19:22:27 +0800

    feat: added navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import {
+    Brain,
+    Briefcase,
+    Folder,
+    FolderGit2,
+    Home,
+    SquarePen,
+    Wrench,
+} from "lucide-react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+
+interface NavItemProps {
+    href: string;
+    label: string;
+    icon: React.ReactNode;
+}
+
+const NavItem = ({ href, label, icon }: NavItemProps) => {
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon" className="w-12 h-12">
+                    <Link
+                        href={href}
+                        className="flex items-center justify-center scale-125"
+                    >
+                        {icon}
+                    </Link>
+                </Button>
+            </TooltipTrigger>
+            <TooltipContent
+                side="bottom"
+                className="bg-white p-2 rounded-lg shadow-lg"
+            >
+                <div className="flex flex-col gap-1">
+                    <p className="font-medium">{label}</p>
+                </div>
+            </TooltipContent>
+        </Tooltip>
+    );
+};
+
+export const Navbar = () => {
+    return (
+        <nav className="sticky top-10 w-fit mx-auto">
+            <div className="bg-secondary rounded-2xl px-4 flex items-center gap-1">
+                <NavItem href="/" label="Home" icon={<Home />} />
+                <NavItem href="/projects" label="Projects" icon={<Folder />} />
+                <NavItem
+                    href="/experience"
+                    label="Experience"
+                    icon={<Briefcase />}
+                />
+                <NavItem href="/tools" label="Tools" icon={<Wrench />} />
+                <NavItem
+                    href="/thoughts"
+                    label="Thoughts"
+                    icon={<SquarePen />}
+                />
+            </div>
+        </nav>
+    );
+};
