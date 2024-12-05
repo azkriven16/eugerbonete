39e391b0f7b1a25c5c7505ff03a141427eef48d4
euger <azkriven16@gmail.com>
2024-12-06 07:54:11 +0800

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://azkriven.vercel.app/",
+            url: "https://eugerbonete.vercel.app/",
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 1,
         },
         {
-            url: "https://azkriven.vercel.app/about",
+            url: "https://eugerbonete.vercel.app/about",
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 0.8,
         },
         {
-            url: "https://azkriven.vercel.app/contact",
+            url: "https://eugerbonete.vercel.app/contact",
             lastModified: new Date(),
             changeFrequency: "yearly",
             priority: 0.5,
