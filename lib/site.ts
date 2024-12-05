635174b26cb03737cb486454e378d08e5210295d
euger <azkriven16@gmail.com>
2024-12-06 07:59:17 +0800

    updated og image

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
     name: "Euger Bonete",
     url: "https://eugerbonete.vercel.app/",
-    ogImage: "https://eugerbonete.vercel.app/api/og",
+    ogImage: "https://eugerbonete.vercel.app/og.png",
     description:
         "Web developer and NISU CICS student. Explore my portfolio of projects, skills, and technical expertise.",
     links: {
